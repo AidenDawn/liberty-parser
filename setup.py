b83521a545cd2fc357b1a45d2e9e70ea41702e62
Thomas Kramer <code@tkramer.ch>
2022-12-02 09:56:27 +0100

    bump version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def readme():
 
 
 setup(name='liberty-parser',
-      version='0.0.14',
+      version='0.0.15',
       description='Liberty format parser.',
       long_description=readme(),
       long_description_content_type="text/markdown",
